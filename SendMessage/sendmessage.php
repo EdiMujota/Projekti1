8eb472a88ee38b78682d8ef59da6a588b355feab
EdiMujota <eduardmujota@gmail.com>
2022-02-14 11:06:18 +0100

    Krijimi dhe lidhja me databaze i pjeses 'Send Message' nga useri

---
+++ SendMessage/sendmessage.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>Feedbacks</title>
+
+    <link rel="stylesheet" href="style.css" />
+
+  </head>
+  <body>
+
+          <form action="" method="post" autocomplete="off">
+            <h3 class="title">"Jepni një mendim për faqen tonë"</h3>
+            <div class="input-container textarea">
+              <textarea name="message" class="input"></textarea>
+              <label for="">Message</label>
+              <span>Message</span>
+            </div>
+            <input type="submit" name="submit" value="Send" class="btn" />
+          </form>
+
+  </body>
+</html>
